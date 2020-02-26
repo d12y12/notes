3d3f50118bd876178cc36e198b31dc8aca2cd38e
Yang DONG <d12y12@hotmail.com>
2020-02-26 23:58:46 +0800

    readthedocs build success, but can not show, back to index

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ release = version
 extensions = [
 ]
 
-master_doc = 'contents'
+master_doc = 'index'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
